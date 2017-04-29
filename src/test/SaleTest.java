4f7c96c5a3a6a72a28e0b145beda3123cd09b76d
koha3496 <koha3496@OSS326-01.stthomas.edu>
2017-04-29 15:34:44 -0500

    Updated SaleTest

--- src/test/SaleTest.java
+++ src/test/SaleTest.java
@@ -2,23 +2,72 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import domain.Copy;
 import domain.Sale;
 import domain.Textbook;
+import lib.StdOut;
 
 public class SaleTest {
+
+	private Copy testCopy;
 	
-	private Sale sale = new Sale();
-	
+	@Before
+	public void setUp() throws Exception {
+		Textbook t = new Textbook("TEST12345","test title", "test author", 1);
+		this.testCopy = new Copy("TEST123456", t);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
-	public void addSaleCopyTest(){
-		sale.addSaleCopy(new Copy("C1T1",
-				new Textbook("ISBN","Book1","Author1",1.0)));
-		assertEquals(sale.copiesInSale(),1);
-		assertEquals(sale.getTotal(),1,.001);
-		
+	public final void testSale() {
+		assertNotNull("new Sale object should not be null",new Sale());
+	}
+
+	@Test
+	public final void testCopiesInSale() {
+		Sale s = new Sale();
+		s.addSaleCopy(this.testCopy);
+		assertEquals("sale should have 1 copy",1,s.copiesInSale());
+	}
+
+	@Test
+	public final void testGetTotal() {
+		Sale s = new Sale();
+		s.addSaleCopy(this.testCopy);
+		StdOut.println(s.getTotal());
+		assertEquals("sale should have total of 1",s.getTotal(),1.0);		
+	}
+
+	@Test
+	public final void testComplete() {
+		try {
+			Sale s = new Sale();
+			s.addSaleCopy(this.testCopy);
+			s.complete();
+		} catch (Exception e) {
+			fail("addSaleCopy and complete should not result in exception: " + e);
+		}
+	}
+
+	@Test
+	public final void testHasCopy() {
+		Sale s = new Sale();
+		s.addSaleCopy(this.testCopy);
+		assertTrue("sale should have copy",s.hasCopy("TEST12345"));
+	}
+
+	@Test
+	public final void testToString() {
+		Sale s = new Sale();
+		s.addSaleCopy(this.testCopy);
+		assertNotNull("toString should not be null",s.toString());
 	}
 
 }
